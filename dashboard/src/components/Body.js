bd384261c81e4f299ebf98a9b95d02fc8d5e4540
ramosv <56368263+ramosv@users.noreply.github.com>
2024-05-10 15:30:21 -0600

    new build

--- dashboard/src/components/Body.js
+++ dashboard/src/components/Body.js
@@ -48,7 +48,7 @@ const columns = [
  * @returns
  */
 export default function Body({ results }) {
-  const [graphs] = useState(["AreaChart", "BarChart"]);
+  const [graphs] = useState(["AreaChart"]);
   const [selectedGraph, setSelectedGraph] = useState(0);
 
   const handleChange = (_, newValue) => {
@@ -179,135 +179,8 @@ function Chart({ name, data }) {
           />
         </AreaChart>
       );
-    case "BarChart":
-      return (
-        <BarChart width={600} height={400} data={data}>
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="name" />
-          <YAxis />
-          <Tooltip />
-          <Legend />
-          <Bar dataKey="pv" fill="#8884d8" />
-          <Bar dataKey="uv" fill="#82ca9d" />
-        </BarChart>
-      );
-    case "LineChart":
-      return (
-        <LineChart
-          width={600}
-          height={400}
-          data={data}
-          margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
-        >
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="name" />
-          <YAxis />
-          <Tooltip />
-          <Legend />
-          <Line type="monotone" dataKey="pv" stroke="#8884d8" />
-          <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
-        </LineChart>
-      );
 
     default:
       return null;
   }
 }
-
-// const data = [
-//   {
-//     name: "Page A",
-//     uv: 4000,
-//     pv: 2400,
-//     amt: 2400,
-//   },
-//   {
-//     name: "Page B",
-//     uv: 3000,
-//     pv: 1398,
-//     amt: 2210,
-//   },
-//   {
-//     name: "Page C",
-//     uv: 2000,
-//     pv: 9800,
-//     amt: 2290,
-//   },
-//   {
-//     name: "Page D",
-//     uv: 2780,
-//     pv: 3908,
-//     amt: 2000,
-//   },
-//   {
-//     name: "Page E",
-//     uv: 1890,
-//     pv: 4800,
-//     amt: 2181,
-//   },
-//   {
-//     name: "Page F",
-//     uv: 2390,
-//     pv: 3800,
-//     amt: 2500,
-//   },
-//   {
-//     name: "Page G",
-//     uv: 3490,
-//     pv: 4300,
-//     amt: 2100,
-//   },
-// ];
-
-// const data01 = [
-//   {
-//     name: "Group A",
-//     value: 400,
-//   },
-//   {
-//     name: "Group B",
-//     value: 300,
-//   },
-//   {
-//     name: "Group C",
-//     value: 300,
-//   },
-//   {
-//     name: "Group D",
-//     value: 200,
-//   },
-//   {
-//     name: "Group E",
-//     value: 278,
-//   },
-//   {
-//     name: "Group F",
-//     value: 189,
-//   },
-// ];
-// const data02 = [
-//   {
-//     name: "Group A",
-//     value: 2400,
-//   },
-//   {
-//     name: "Group B",
-//     value: 4567,
-//   },
-//   {
-//     name: "Group C",
-//     value: 1398,
-//   },
-//   {
-//     name: "Group D",
-//     value: 9800,
-//   },
-//   {
-//     name: "Group E",
-//     value: 3908,
-//   },
-//   {
-//     name: "Group F",
-//     value: 4800,
-//   },
-// ];
